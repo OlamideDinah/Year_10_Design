3cda8e3a780d830bde85e4f4b94bb409a2311aa5
Y9Design-ODIN <55104062+Y9Design-ODIN@users.noreply.github.com>
2020-11-06 20:07:57 -0500

    Update example.js

--- Project_1_Work/example.js
+++ Project_1_Work/example.js
@@ -10,47 +10,39 @@
 	}));
 }).then(data => { // data from this api is already parsed (in an array) makes this process super easy
 
-    // Below section works but isn't being used right now
+   
+    var news = data[1] // so this variable is being set to the data returned from my first fetch
+    title = news[0].headline
+ 
+    currency = 'usd' // used later on - the currency used on all pages
+
+    document.getElementById("source").innerHTML = 'Source' + ": " + news[0].source // calling an element from JS object
+    link = JSON.stringify(news[0].url); // turning link into a string - used just below to act as a hyperlink
+
+    var url = document.getElementById('url');
+    url.innerHTML = '<a href=' + link + '>' + title + '</a>'; // hyperlinked title on html page
+
+
+    var prices = data[2] // setting prices to the third api fetch
+    document.getElementById("targethigh").innerHTML = 'Highest Price Estimate' + ": " + prices.targetHigh + " " + currency // the first element in ok
+    document.getElementById("targetlow").innerHTML = 'Lowest Price Estimate' + ": " + prices.targetLow + " " + currency // no need to delve further into the object its only 1 layer
+    document.getElementById("targetmean").innerHTML = 'Mean Price Estimate' + ": " + prices.targetMean + " " + currency
+
+
+
+    console.log(data[2]);
+
+}).catch(error => {
+	// if there's an error, log it
+	console.log(error);``
+});
+
+
+
+ // Below section works but isn't being used right now
 
     /* var earnings = data[0] // so this variable is being set to the data returned from my first fetch
     document.getElementById("actual").innerHTML = 'Actual Value' + ": " + earnings[0].actual // the first element in ok
     document.getElementById("estimated").innerHTML = 'Estimated Value' + ": " + earnings[0].estimate
     document.getElementById("symbol").innerHTML = 'Company Symbol' + ": " + earnings[0].symbol
 */
-    var news = data[1] // so this variable is being set to the data returned from my first fetch
-    title = news[0].headline
- 
-    currency = 'usd'
-
-    document.getElementById("source").innerHTML = 'Source' + ": " + news[0].source
-    link = JSON.stringify(news[0].url);
-
-    var url = document.getElementById('url');
-    url.innerHTML = '<a href=' + link + '>' + title + '</a>';
-
-
-    var prices = data[2]
-    document.getElementById("targethigh").innerHTML = 'Highest Price Estimate' + ": " + prices.targetHigh + " " + currency // the first element in ok
-    document.getElementById("targetlow").innerHTML = 'Lowest Price Estimate' + ": " + prices.targetLow + " " + currency // no need to delve further into the object its only 1 layer
-    document.getElementById("targetmean").innerHTML = 'Mean Price Estimate' + ": " + prices.targetMean + " " + currency
-
-
-  
-
-
-
-   /* var ok = data[0] // so this variable is being set to the data returned from my first fetch
-    document.getElementById("actual").innerHTML = 'Actual Value' + ": " + ok[0].actual // the first element in ok
-    document.getElementById("estimated").innerHTML = 'Estimated Value' + ": " + ok[0].estimate
-    document.getElementById("symbol").innerHTML = 'Company Symbol' + ": " + ok[0].symbol
-    */
-    console.log(data[2]);
-
-}).catch(error => {
-	// if there's an error, log it
-	console.log(error);
-});
-
-
-
-
